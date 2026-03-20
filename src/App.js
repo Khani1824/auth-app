b9eec52d58b4f9a4473e298a93efed2f6ace1b8f
Khani1824 <KhanimamboMaluleke@gmail.com>
2026-03-20 22:28:32 +0200

    Login and log pout without any authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import './App.css';
-import { Container, Button, Navbar, Card, Form } from 'react-bootstrap';
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Home from "./Home";
 
 function App() {
   return (
-   <div className="d-flex justify-content-center align-items-center vh-100">
-      <Container className='text-center border border-dark' style={{ width: '350px' }}>
-         <h1>Sign In</h1>
-        <Form className=" p-1 text-start">Email address:</Form>
-        <Card className = "p-1 text-start">
-          <Form.Control type="email" placeholder="Enter email"/>
-        </Card>
-        <Form className="p-1 text-start">Password:</Form>
-        <Card className="p-1 text-start">
-          <Form.Control type="password" placeholder="Password"/>
-        </Card>
-        <Button className="p-2 text-start">Log In</Button>
-      </Container>
-   </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
