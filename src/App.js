6afd3c7fd3a32db04ea773b5daf837c0fd433c2f
Khani1824 <KhanimamboMaluleke@gmail.com>
2026-03-20 21:18:59 +0200

    Place holders implemented

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ function App() {
   return (
    <div className="d-flex justify-content-center align-items-center vh-100">
       <Container className='text-center border border-dark' style={{ width: '350px' }}>
-        <Form className="text-start">Username:</Form>
-        <Card>Enter your Username</Card>
-        <Form className="text-start">Password:</Form>
-        <Card>Enter your Password</Card>
-        <Button> Log In</Button>
+         <h1>Sign In</h1>
+        <Form className=" p-1 text-start">Email address:</Form>
+        <Card className = "p-1 text-start">
+          <Form.Control type="email" placeholder="Enter email"/>
+        </Card>
+        <Form className="p-1 text-start">Password:</Form>
+        <Card className="p-1 text-start">
+          <Form.Control type="password" placeholder="Password"/>
+        </Card>
+        <Button className="p-2 text-start">Log In</Button>
       </Container>
    </div>
   );
