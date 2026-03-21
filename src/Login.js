63bd703eaf691893681eebbe833019525b0e6e91
Khani1824 <KhanimamboMaluleke@gmail.com>
2026-03-21 10:39:46 +0200

    Authentication implimented

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,27 @@
 import "./App.css";
-import { Container, Button, Navbar, Card, Form } from "react-bootstrap";
+import { Container, Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { USERS } from "./users";
+import { useState } from "react";
 
 function Login() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    const foundUser = USERS.find(
+      (user) => user.email === email && user.password === password,
+    );
+
+    if (foundUser) {
+      navigate("/home");
+    } else {
+      setError("Incorrect email or password. Please try again.");
+    }
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
       <Container
@@ -13,13 +31,22 @@ function Login() {
         <h1>Sign In</h1>
         <Form className="p-1 text-start">
           <Form className=" p-1 text-start">Email address:</Form>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form>
         <Form className="p-1 text-start mb-2">
           <Form className="p-1 text-start">Password:</Form>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form>
-        <Button className="mt-2 w-100" onClick={() => navigate("/home")}>
+        {error && <p style={{ color: "red", fontSize: "14px" }}>{error}</p>}
+        <Button className="mt-2 w-100" onClick={handleLogin}>
           Log In
         </Button>
       </Container>
